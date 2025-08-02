c4ed35d87fb6fa4a46189d4641953543910eaf95
Jasil-kk <jasikk2522@gmail.com>
2025-08-02 11:38:42 +0530

    home:signup-newsletter section ui added

---
+++ assets/js/swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".testimonialSwiper", {
+  loop: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
